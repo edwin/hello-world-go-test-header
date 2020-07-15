627bd4db5efa99d6e4f233e8a99a4318200bec9c
Muhammad Edwin <edwin@baculsoft.com>
2020-07-15 12:51:43 +0700

    update main.go
    key header upper-lower

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Print("inside here")
 	http.HandleFunc("/", get)
 	http.ListenAndServe(":8080", nil)
 }
@@ -14,11 +15,12 @@ func get(w http.ResponseWriter, r *http.Request) {
 
 	// json header default
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("HeaDer01", "Satu")
+	w.Header().Set("HeaDer07", "Tujuh Tujuh")
 	w.Header().Set("HeadeR02", "DUA")
 	w.Header().Set("HeAder03", "tIGA")
 	w.Header().Set("HEader04", "EmPaT")
 	w.Header().Set("HEAdER05", "limA")
+	w.Header().Set("HEAdER06-HEADER-HeaRER-ABC-xYZ", "ENAM")
 
 	fmt.Fprint(w, "{\"hello\":\"world\"}")
 	return
